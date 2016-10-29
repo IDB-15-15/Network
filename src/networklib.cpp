92ea74f712379f8418ff42ee7d1f9e6f5b15716c
dimasbr <strkovdima@gmail.com>
2016-10-29 22:00:10 +0300

    test

--- src/networklib.cpp
+++ src/networklib.cpp
@@ -11,7 +11,8 @@ const std::string shifr="N";
 
 
 
-std::string http(std::string host, std::string page, bool* err){
+std::string http(std::string host, std::string page, bool* err)
+	{
     using namespace boost::asio;
     io_service service;
     ip::tcp::resolver resolver(service);
@@ -26,93 +27,107 @@ std::string http(std::string host, std::string page, bool* err){
     sock.connect(ep, ec);
 
     if (ec==0){
-    //std::cout<<"Get request on the way!";
-    std::string req="GET "+page+" HTTP/1.1\r\n"+"Host: "+host+
-                    "\r\nUser-Agent: "+::browser_name+"("+::platform+", "+::shifr+", ru)"+
-                    "\r\nAccept: text/html"+"\r\nContent-Length: 0\r\n"+"Connection: close\r\n\r\n";
-    std::cerr << __FILE__ << ':' << __LINE__ << std::endl;
-    boost::asio::streambuf buff;
-    read_until(sock, buff , "\r\n\r\n");
-    std::vector<std::string> header;
-    std::istream str(&buff);
-    std::string http_version;
-    str>> http_version;
-    //std::cout<<http_version;
-    header.push_back(http_version);    //1-ый эл-т хедера - версия HTTP
-    unsigned int status_code;
-    str >> status_code;
-    std::string sti=std::to_string(status_code);
-    //std::cout<<sti;
-    header.push_back(sti);    //2-ой-статус подключения в виде кода
-    std::string status_message;
-    std::getline(str, status_message);
-    //std::cout<<status_message;
-    header.push_back(http_version);     //3-ий-в виде сообщения
-    std::string now;
-    while (std::getline(str, now) && now != "\r"){
-      header.push_back(now);
-	  std::cout<<now;
-    }
-std::cerr << __FILE__ << ':' << __LINE__ << std::endl;
-std::cerr << __FILE__ << ':' << __LINE__ << std::endl;
-    str.clear();
-    int k =3;
-    int size;
-    std::string st;
-    while (k<header.size()){
-       	st=header[k];
-        st.substr(0, 16);
-        if (st=="Content-Length: "){
-            std::string::size_type sz; 
-			size=std::stoi (header[k], &sz);        
+    	//std::cout<<"Get request on the way!";
+    	std::string req="GET " + page + " HTTP/1.1\r\n" + "Host: " + host +
+                    	"\r\nUser-Agent: " + ::browser_name + "(" + ::platform + ", " + ::shifr + ", ru)" +
+                    	"\r\nAccept: text/html" + "\r\nContent-Length: 0\r\n" + "Connection: close\r\n\r\n";
+
+    	boost::asio::streambuf buff;
+    	read_until(sock, buff , "\r\n\r\n");
+    	std::vector<std::string> header;
+    	std::istream str(&buff);
+    	std::string http_version;
+    	str>> http_version;
+    	//std::cout<<http_version;
+    	header.push_back(http_version);    //1-ый эл-т хедера - версия HTTP
+
+    	unsigned int status_code;
+    	str >> status_code;
+    	std::string sti = std::to_string(status_code);
+    	//std::cout<<sti;
+    	header.push_back(sti);    //2-ой-статус подключения в виде кода
+
+    	std::string status_message;
+    	std::getline(str, status_message);
+    	//std::cout<<status_message;
+    	header.push_back(http_version);     //3-ий-в виде сообщения
+
+    	std::string now;
+    	while (std::getline(str, now) && now != "\r")
+			{
+      		header.push_back(now);
+	  		std::cout<<now;
+    	}
+
+		std::cerr << __FILE__ << ':' << __LINE__ << std::endl;
+		std::cerr << __FILE__ << ':' << __LINE__ << std::endl;
+    	str.clear();
+    	int k =3;
+    	int size;
+    	std::string st;
+    	while (k<header.size())
+			{
+       		st=header[k];
+        	st.substr(0, 16);
+        	if (st=="Content-Length: ")
+				{
+            	std::string::size_type sz; 
+				size=std::stoi (header[k], &sz);        
+				}
+			k++; 
+    	}
+
+		boost::shared_ptr<unsigned char[]> body = boost::make_shared<unsigned char[]>(size);
+       	boost::system::error_code error;
+    	read(sock, buffer(body.get(), size), error);
+
+    	//sock.shutdown(ip::tcp::socket::shutdown_receive);
+    	sock.close();
+    	std::string res((char*)body.get());
+    	return res;
 		}
-		k++; 
-    }
-	boost::shared_ptr<unsigned char[]> body=boost::make_shared<unsigned char[]>(size);
-    
-    boost::system::error_code error;
-    read(sock, buffer(body.get(), size), error);
-    //sock.shutdown(ip::tcp::socket::shutdown_receive);
-    sock.close();
-    std::string res((char*)body.get());
-    return res;}
 
-    else {*err=true; return ec.message();} //Здесь надо будет возвращать код ошибки
-}
+    else 
+		{
+		*err=true; return ec.message();
+		}                                                 //Здесь надо будет возвращать код ошибки
+	}
 
-NetworkRes get(std::string site){
+NetworkRes get(std::string site)
+	{
     using namespace std;
     //std::cout<<"Maybe it will work";
 
-    string str_http="http://";
-    string str_https="https://";
-    string page="";
-
+    string str_http = "http://";
+    string str_https = "https://";
+    string page = "";
     NetworkRes result;                        //Возвращаемый экземпляр класса
 
     bool right=true;
     bool err=false;
+
     for (int i=8; i<site.length(); i++)
-    {                                         //Проверяем на наличие адреса страницы
-            if (site[i]=='/') right=false;
-    }
+    	{                                         //Проверяем на наличие адреса страницы
+        if (site[i]=='/') right=false;
+    	}
     if (right)
-    {                                //И добавляем, если надо
-            page="/";
-    }
+    	{                                //И добавляем, если надо
+        page="/";
+    	}
 
     //if (site.find(str_http)!=site.npos)       //Работа по HTTP
     //{
             
             //std::cout<<"Get foo enabled!";
-            site.erase(0,7);
-            std::string::size_type sl = site.find('/');
-            page=site.substr(sl);
-            int pl = (int)sl;
-            site.erase(pl,site.length()-1);
-            string res=http(site, page, &err);
-            result.set_mode(1);
-            result.push(res);		
+	site.erase(0,7);
+    std::string::size_type sl = site.find('/');
+    page=site.substr(sl);
+    int pl = (int)sl;
+    site.erase(pl,site.length()-1);
+
+    string res = http(site, page, &err);
+    result.set_mode(1);
+    result.push(res);		
     //}
 //    if (site.find(str_https)!=site.npos){       //Работа по HTTP
 //            site.erase(0,8);
@@ -125,6 +140,6 @@ NetworkRes get(std::string site){
 //            result.push(res);
 
 //        }
-    return result;
+	return result;
 
 }
