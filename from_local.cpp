1336061df23166d4aada1aebca493afbaf5126a2
Nekit228 <panatikin@gmail.com>
2016-11-27 23:12:35 +0300

    Add files via upload

--- from_local.cpp
+++ from_local.cpp
@@ -2,26 +2,33 @@
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <iostream>
+#include <string>
 
-using namespace std;
+//using namespace std;
 
-int main(){
+//boost::any local(std::string host, std::string page, bool* err, std::map<std::string, std::string>& header, char *body_)
+boost::any local(std::string local_url){
     using namespace boost::interprocess;
+    const char *name;
+    name = local_url.c_str();
 
-    char *name;
-    cout << "n = ";
-    cin >> name;
-    const char *file_name = name;//("C:/Users/Nekit/Documents/file.txt");
-    //const std::size_t file_size = 10000;
+    const char *file_name = name;
 
     file_mapping m_file(file_name, read_only);
     mapped_region region(m_file, read_only);
 
-    //Get the address of the mapped region
     void * addr       = region.get_address();
     std::size_t size  = region.get_size();
 
-    cout.write(static_cast<const char *> (addr), size);
-    cout<<"ok";
+    std::cout.write(static_cast<const char *> (addr), size);
+    return 0;
+}
+
+int main(){
+    std::string str;
+    std::cout << "n = ";
+    std::getline(std::cin, str);
+    local(str);
+    //"C:/Users/Nekit/Documents/Qt projects/from_local/networklib.h";
     return 0;
 }
