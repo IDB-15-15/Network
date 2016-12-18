95773db48d27bf65b7fdc2ebd87823260e65085f
Nekit228 <panatikin@gmail.com>
2016-12-19 01:38:18 +0300

    Add files via upload

--- src/locallib.cpp
+++ src/locallib.cpp
@@ -2,6 +2,7 @@
 #include "networkres.h"
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/exceptions.hpp>
 #include <string>
 #include <utility>
 #include <memory>
@@ -9,17 +10,27 @@
 NetworkRes local(std::string local_url)
 {
     using namespace boost::interprocess;
-    const char *file_name = local_url.c_str();	//get file name
+    const char *file_name = local_url.c_str();
+    NetworkRes res;
+    std::shared_ptr<std::string> error_ptr;
 
-    file_mapping m_file(file_name, read_only);  //read file
-    mapped_region region(m_file, read_only);    //push it to region
+    try{
+        file_mapping m_file(file_name, read_only);  //read file
+        mapped_region region(m_file, read_only);    //push it to region
 
-    NetworkRes res;                             //create object of save result class
+        res.res_arr = (static_cast<const char *> (region.get_address()));   //save array's address
+        res.size = region.get_size();                                       //save array's size
+        res.res = std::make_shared<std::pair<file_mapping, mapped_region>>
+                (std::move (m_file), std::move (region));                   //save array
+    }
+    catch(interprocess_exception &e){
+        error_ptr = std::make_shared<std::string>                           //create error's message
+                ("<html>" + static_cast<std::string> (e.what()) + "</html>");
 
-    res.res_arr = (static_cast<const char *> (region.get_address()));   //save array's address
-    res.size = region.get_size();                                       //save size
-    res.res = std::make_shared<std::pair<file_mapping, mapped_region>>
-            (std::move (m_file), std::move (region));                   //save array
+        res.res = error_ptr;                //save error's message
+        res.size = error_ptr->size();       //save error's message size
+        res.res_arr = error_ptr->c_str();   //save error's message address
+    }
 
     return res;
 }
