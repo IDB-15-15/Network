1b0d41acaf0920ff34d630067dc468eb5f40ce8e
Nekit228 <panatikin@gmail.com>
2016-12-25 21:13:27 +0300

    Update tool_Networklib.cpp

--- src/tool_Networklib.cpp
+++ src/tool_Networklib.cpp
@@ -8,11 +8,11 @@ int main(int argc, char **argv) {
 //    std::string site = "file://C:/Master/Network/src/locallib.cpp";
 //    Network::NetworkRes res = Network::give_result(site);
 //    std::cout << res.get_res_arr() << std::endl;
-	try{
-		std::string site=argv[1];
-		auto res=Network::give_result(site);
-		std::cout<<res.res_arr;
-	} catch(std::exception &e) {
-		std::cerr<<"ALARM!!! "<<e.what()<<std::endl;
-		}
+    try{
+        std::string site = argv[1];
+	auto res = Network::give_result(site);
+	std::cout << res.res_arr;
+    } catch(std::exception &e) {
+	std::cerr << "ALARM!!! " << e.what() << std::endl;
+    }
 }
